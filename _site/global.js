4c87beb26686f6e17e532e59c48f886330e24f33
Madam Ella <madamellamalala@gmail.com>
2025-12-02 15:58:45 +0200

    ,

---
+++ _site/global.js
@@ -0,0 +1,32 @@
+// alert("There!");
+
+var vLa, vLo;
+var vU = "https://nominatim.openstreetmap.org/reverse?lat="+vLa+"&lon="+vLo+"&format=json";
+doA();
+
+function doA() {
+	var vULa = localStorage.getItem("vULa");
+	if(vULa) { doD(); }
+	else { if(navigator.geolocation) { navigator.geolocation.getCurrentPosition(doB); } }
+}
+function doB(position) {
+	vLa = position.coords.latitude;
+	vLo = position.coords.longitude;
+	localStorage.setItem("vULa", JSON.stringify(vLa));
+	localStorage.setItem("vULo", JSON.stringify(vLo));
+
+//	doC(vLa, vLo);
+}
+function doMap(vLa, vLo) {
+	fetch(vU)
+		.then(response => response.json())
+		.then(data => {
+			if(data && data.display_name) { console.log(data.display_name); }
+		});
+}
+function doD() {
+	vLa = localStorage.getItem("vULa");
+	vLo = localStorage.getItem("vULo");
+	
+	doMap(vLa, vLo);
+}
